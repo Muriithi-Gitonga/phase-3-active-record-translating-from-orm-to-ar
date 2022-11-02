fe960f79bf0399ebf056a93f021a7fdab55d4df1
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-11-02 22:13:28 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,23 @@
-class Dog
+require "active_record"
+
+# setup a database connection
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database:"db/test.sqlite"
+)
+
+# create a test table
+
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+)
+SQL
+
+
+
+class Dog < ActiveRecord::Base
 end
